b05f8cae9ca560b7c2f4eeeb1e79a858396c3bfd
fjakrapan <jakrapanworkshop64@gmail.com>
2024-07-07 09:58:53 +0700

    chang with leader

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,76 +11,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        //addbar คือข้างบน
-        title: Center(child: Text('Home')),
-      ),
-      body: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              width: 170,
-              padding: const EdgeInsets.all(8.0),
-              child: const Column(
+            const SizedBox(
+              width: 230,
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Strawberry Pavlova',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  Text(
+                    "Strawberry Pavlova",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 10),
                   Text(
-                    'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.',
-                    style: TextStyle(fontSize: 10),
-                  ),
-                  SizedBox(height: 10),
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisl nec condimentum"),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Row(
                         children: [
-                          Icon(Icons.star, color: Colors.yellow, size: 10),
-                          Icon(Icons.star, color: Colors.yellow, size: 10),
-                          Icon(Icons.star, color: Colors.yellow, size: 10),
-                          Icon(Icons.star, color: Colors.yellow, size: 10),
-                          Icon(Icons.star, color: Colors.yellow, size: 10),
+                          Icon(
+                            Icons.star,
+                            size: 14,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 14,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 14,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 14,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 14,
+                          ),
                         ],
                       ),
-                      SizedBox(width: 10),
-                      Text('170 Reviews', style: TextStyle(fontSize: 10)),
+                      Text("170 Reviews"),
                     ],
                   ),
-                  SizedBox(height: 10),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Column(
                         children: [
-                          Icon(Icons.kitchen, color: Colors.green, size: 14),
-                          Text('PREP:', style: TextStyle(fontSize: 10)),
-                          Text('25 min', style: TextStyle(fontSize: 10)),
+                          Icon(Icons.nat),
+                          Text("Test"),
+                          Text("test"),
                         ],
                       ),
                       Column(
                         children: [
-                          Icon(Icons.timer, color: Colors.green, size: 14),
-                          Text('COOK:', style: TextStyle(fontSize: 10)),
-                          Text('1 hr', style: TextStyle(fontSize: 10)),
+                          Icon(Icons.arrow_back_rounded),
+                          Text("Test"),
+                          Text("test"),
                         ],
                       ),
                       Column(
                         children: [
-                          Icon(Icons.restaurant, color: Colors.green, size: 14),
-                          Text('FEEDS:', style: TextStyle(fontSize: 10)),
-                          Text('4-6', style: TextStyle(fontSize: 10)),
+                          Icon(Icons.turn_slight_left_outlined),
+                          Text("Test"),
+                          Text("test"),
                         ],
                       ),
                     ],
@@ -89,20 +90,14 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(width: 10),
-                  Image.asset(
-                    'Images/a.jpg',
-                    width: 1000,
-                    height: 1000,
-                    fit: BoxFit.cover,
-                  ),
-                ],
+              child: Container(
+                // color: Colors.blue,
+                child: Image.asset('images/a.jpg'),
               ),
             ),
-          ]),
+          ],
+        ),
+      ),
     );
   }
 }
